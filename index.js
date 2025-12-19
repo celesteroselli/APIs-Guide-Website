9fc11322ec3e49e36cc6157f4fbefa91280a7633
108charlotte <108sarahsmith@gmail.com>
2025-12-19 16:38:05 -0500

    currently broken bc await issues but added instructions on using API

---
+++ index.js
@@ -0,0 +1,9 @@
+const catapi_call = await fetch('https://api.thecatapi.com/v1/images/search')
+const catapi_json = await catapi_call.json()
+const img_url = catapi_json[0].url
+const img_width = catapi_json[0].width
+const img_height = catapi_json[0].height
+const image_elem = document.getElementById('catapi_image_elem')
+image_elem.src = img_url
+image_elem.width = img_width
+image_elem.height = img_height
