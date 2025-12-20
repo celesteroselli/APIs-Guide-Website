02b720eab03849aacd99fc9a258be7637456b69a
108charlotte <108sarahsmith@gmail.com>
2025-12-20 15:11:09 -0500

    using an api page now works, created outline for using an api key page

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const catapi_call = await fetch('https://api.thecatapi.com/v1/images/search')
-const catapi_json = await catapi_call.json()
-const img_url = catapi_json[0].url
-const img_width = catapi_json[0].width
-const img_height = catapi_json[0].height
-const image_elem = document.getElementById('catapi_image_elem')
-image_elem.src = img_url
-image_elem.width = img_width
-image_elem.height = img_height
+document.addEventListener('DOMContentLoaded', setRandomCatImage)
+// need an async function for await to work
+async function setRandomCatImage() {
+    const catapi_call = await fetch('https://api.thecatapi.com/v1/images/search')
+    const catapi_json = await catapi_call.json()
+    const img_url = catapi_json[0].url
+    const img_width = catapi_json[0].width
+    const img_height = catapi_json[0].height
+    const image_elem = document.getElementById('catapi_image_elem')
+    image_elem.src = img_url
+}
